1f1dceb1e2b2dba3da271f5140471b27ba64aeb8
verciel <137381361+verciel@users.noreply.github.com>
2026-08-05 00:21:31 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n=word1.length(), m=word2.length(), i=0, j=0;
+        string ans="";
+        while(i<n && j<m)
+        {
+            ans+=word1[i++];
+            ans+=word2[j++];
+        }
+
+        while(i<n) ans+=word1[i++];
+        while(j<m) ans+=word2[j++];
+    
+        return ans;
+    }
+};
